a727d91b061895b57bf354efdb2ea9230c1542be
Sy Dau <SyDung.Dau@fdmgroup.com>
2023-06-17 12:15:30 -0400

    add global dark mode theming

---
+++ frontend/src/themes/darkTheme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const darkTheme = createTheme({
+    typography: {
+        allVariants: {
+            color: '#ffffff',
+        },
+    },
+    palette: {
+        primary: {
+            main: '#1F213A',
+        },
+        background: {
+            default: '#141625',
+            paper: '#1F213A'
+        },
+    },
+});
+
+export default darkTheme;
